f44251f8af7cfab481c870b3d07a6e3cad7e8af8
Gianluca Massera <gianluca@redberrydigital.com>
2015-10-13 17:37:19 +0100

    starting to prepare the classes for supporting PARSE types

--- qparsetypes.h
+++ qparsetypes.h
@@ -1,12 +1,27 @@
 #ifndef QPARSETYPES_H
 #define QPARSETYPES_H
 
+#include <QJsonObject>
+#include <QDateTime>
+
 //! \file This file contains the classes for handle easily the PARSE data types
 
 //! The Date type on PARSE
 class QParseDate {
 public:
-
+	//! construct a QParseDate from the Json PARSE representation
+	QParseDate( QJsonObject fromParse );
+	//! construct a QParseDate from a QDateTime object
+	QParseDate( QDateTime fromDateTime );
+	//! export into Json PARSE representation
+	QJsonObject toJson();
+	//! export into QDateTime
+	QDateTime toDateTime();
+private:
+	//! iso representation for PARSE
+	QString iso;
+	//! QDateTime representation
+	QDateTime dateTime;
 };
 
 #endif // QPARSETYPES_H
